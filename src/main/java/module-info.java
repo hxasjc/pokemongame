0d5d01e272a0c694f3ac529fce4d4e5a442be494
hxasjc <karlvonweyhe@gmail.com>
2023-06-21 14:26:02 -0400

    update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,6 @@ module hxasjc.pokemongame {
 
     opens hxasjc.pokemongame to javafx.fxml;
     exports hxasjc.pokemongame;
-    exports hxasjc.pokemongame.demo;
-    opens hxasjc.pokemongame.demo to javafx.fxml;
     opens hxasjc.pokemongame.components to javafx.fxml;
     exports hxasjc.pokemongame.components;
 }
